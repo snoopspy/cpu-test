247432a4b2ba26bb75aa0669f34744c3302c939a
gilgil <gilgil@gilgil.net>
2021-06-22 20:23:26 +0900

    Replace space with tab

--- main.cpp
+++ main.cpp
@@ -11,51 +11,50 @@ typedef chrono::high_resolution_clock::duration Diff;
 typedef chrono::high_resolution_clock Timer;
 
 struct Param {
-  int threadCnt_{1};
-  Diff duration_{chrono::seconds(10)};
+	int threadCnt_{1};
+	Diff duration_{chrono::seconds(10)};
 
-  bool parse(int argc, char* argv[]) {
-    if (argc != 3) {
-      usage();
-      return false;
-    }
-    threadCnt_ = stoi(argv[1]);
-    duration_ = Diff(strtoll(argv[2], nullptr, 0) * 1000000000);
-    return true;
-  }
+	bool parse(int argc, char* argv[]) {
+		if (argc != 3) {
+			usage();
+			return false;
+		}
+		threadCnt_ = stoi(argv[1]);
+		duration_ = Diff(strtoll(argv[2], nullptr, 0) * 1000000000);
+		return true;
+	}
 
-  void usage() {
-    cout << "cpu_test <thread count> <duration(sec)>\n";
-    cout << "example : cpu_test 4 10\n";
-  }
+	void usage() {
+		cout << "syntax: cpu_test <thread count> <duration(sec)>\n";
+		cout << "sample: cpu_test 4 10\n";
+	}
 };
 
 void run(Diff* duration) {
-  Timer timer;
-  Clock begin = timer.now();
-  while (true) {
-    Clock now = timer.now();
-    Diff diff = now - begin;
-    if (diff > *duration)
-      break;
-  }
+	Timer timer;
+	Clock begin = timer.now();
+	while (true) {
+		Clock now = timer.now();
+		Diff diff = now - begin;
+		if (diff > *duration)
+			break;
+	}
 }
 
-int main(int argc, char *argv[])
-{
-  Param param;
-  if (!param.parse(argc, argv))
-    return 0;
+int main(int argc, char *argv[]) {
+	Param param;
+	if (!param.parse(argc, argv))
+		return 0;
 
-  vector<thread*> threads;
-  for (int i = 0; i < param.threadCnt_; i++)
-    threads.push_back(new thread(run, &param.duration_));
+	vector<thread*> threads;
+	for (int i = 0; i < param.threadCnt_; i++)
+		threads.push_back(new thread(run, &param.duration_));
 
-  for (thread* thrd: threads)
-    thrd->join();
+	for (thread* thrd: threads)
+		thrd->join();
 
-  for (thread* thrd: threads)
-    delete thrd;
+	for (thread* thrd: threads)
+		delete thrd;
 
-  return 0;
+	return 0;
 }
